5cd98bb47bac855f3065db178275ee4ea7cfffac
Judith Meißner <meissner00@gmx.de>
2020-12-07 17:43:42 +0100

    Datei anlegen

---
+++ GGT.java
@@ -0,0 +1,28 @@
+class GGT {
+	public static void main(String args[]) {
+		int m = Integer.parseInt(args[0]);
+		int n = Integer.parseInt(args[1]);
+
+		if (m < 0 || n < 0) {
+			System.out.println("Nur positive Zahlen erlaubt!");
+		} else {
+
+			do {
+
+				if (m < n) {
+					int k = n;
+					n = m;
+					m = k;
+				}
+
+				int r = m - n;
+				m = n;
+				n = r;
+			}
+
+			while (n > 0);
+
+			System.out.println("ggT(" + args[0] + "," + args[1] + ") = " + m);
+		}
+	}
+}
